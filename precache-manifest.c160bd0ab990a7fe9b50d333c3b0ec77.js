14aa8d2eb042adc60508f8d79be8e7328a84be9a
Bowser <36619895+TimotheeSai@users.noreply.github.com>
2020-04-19 12:04:05 +0200

    Updates

---
+++ precache-manifest.c160bd0ab990a7fe9b50d333c3b0ec77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36e3e7daa4db21e1158553d6fb3b422",
+    "url": "/market/index.html"
+  },
+  {
+    "revision": "e0c45b7d631cdc233502",
+    "url": "/market/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9452b55997177904244",
+    "url": "/market/static/js/2.25da717c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/market/static/js/2.25da717c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c45b7d631cdc233502",
+    "url": "/market/static/js/main.fbfae2d6.chunk.js"
+  },
+  {
+    "revision": "9e5fd4a7a2d2532f9f93",
+    "url": "/market/static/js/runtime-main.96be51a2.js"
+  },
+  {
+    "revision": "5f5494c49d6b8daa1add1e9dc77afb31",
+    "url": "/market/static/media/background_vert.5f5494c4.jpg"
+  }
+]);
